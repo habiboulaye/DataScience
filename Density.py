0f96b22a3b57d02fe8bfb4e062f0926e431d567d
habiboulaye <habiboulaye@gmail.com>
2013-08-04 20:39:36 +0200

    Update and rename mapDensity.py to Density.py

--- Density.py
+++ Density.py
@@ -4,7 +4,10 @@
 
 Consider a text file containing 100 million entries as comma separated values describing
 points reported from cabs across Paris e.g. 51.510772,-0.114756.
-Write a tool that processes the points and generates an image showing their density e.g. a heatmap.
+
+pyDensity.py program processes the points and generates an image showing 
+their density heatmap with zooming functionality.
+
 
 """
 import os
