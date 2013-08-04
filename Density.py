286a5bbf0573a5d6fb8b06b87564d8c3007ef467
habiboulaye <habiboulaye@gmail.com>
2013-08-04 20:39:58 +0200

    Update Density.py

--- Density.py
+++ Density.py
@@ -5,7 +5,7 @@
 Consider a text file containing 100 million entries as comma separated values describing
 points reported from cabs across Paris e.g. 51.510772,-0.114756.
 
-pyDensity.py program processes the points and generates an image showing 
+Density.py program processes the points and generates an image showing 
 their density heatmap with zooming functionality.
 
 
